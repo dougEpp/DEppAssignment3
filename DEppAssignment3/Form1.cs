f0fe7b828103f25cf8aadada6827951d04ae70d5
dougEpp <doug.epp@gmail.com>
2015-10-28 18:56:41 -0400

    Created the project

---
+++ DEppAssignment3/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DEppAssignment3
+{
+    public partial class Form1 : Form
+    {
+        const int DEFAULT_NUM_COLUMNS = 4;
+        const int DEFAULT_NUM_ROWS = 4;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
