d4895d6ec65869bb329a01c70723db7c15c172ae
dougEpp <doug.epp@gmail.com>
2015-10-28 19:04:08 -0400

    Added Tile class and grid of tiles

--- DEppAssignment3/Form1.cs
+++ DEppAssignment3/Form1.cs
@@ -14,9 +14,37 @@ namespace DEppAssignment3
     {
         const int DEFAULT_NUM_COLUMNS = 4;
         const int DEFAULT_NUM_ROWS = 4;
+        const int HEIGHT = 30;
+        const int WIDTH = 30;
+        const int TOP = 10;
+        const int LEFT = 10;
+
+        Tile[,] tiles;
         public Form1()
         {
             InitializeComponent();
+
+            int num_rows = DEFAULT_NUM_ROWS;
+            int num_columns = DEFAULT_NUM_COLUMNS;
+
+            tiles = new Tile[num_rows, num_columns];
+
+            int x; 
+            int y = TOP;
+            for (int i = 0; i < DEFAULT_NUM_ROWS; i++)
+            {
+                x = LEFT;
+                for (int j = 0; j < DEFAULT_NUM_COLUMNS; j++)
+                {
+                    tiles[i,j] = new Tile(HEIGHT, WIDTH, y, x);
+                    x += WIDTH;
+                }
+                y += HEIGHT;
+            }
+            foreach(Tile tile in tiles)
+            {
+                Controls.Add(tile);
+            }
         }
     }
 }
---
+++ DEppAssignment3/Tile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DEppAssignment3
+{
+    class Tile : Button
+    {
+        
+        public Tile(int height, int width, int top, int left)
+        {
+            this.Height = height;
+            this.Width = width;
+            this.Top = top;
+            this.Left = left;
+        }
+    }
+}
